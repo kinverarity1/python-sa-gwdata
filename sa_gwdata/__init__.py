7cbb46e2bc27c404706d76835245c150e5a25e21
Kent Inverarity <kinverarity@hotmail.com>
2020-01-07 16:35:53 +1030

    Make objects visible for pdoc

--- sa_gwdata/__init__.py
+++ sa_gwdata/__init__.py
@@ -3,3 +3,15 @@ from sa_gwdata.sarig import *
 from sa_gwdata.identifiers import *
 
 __version__ = "0.6.0"
+
+__all__ = (
+    "UnitNo",
+    "ObsNo",
+    "Well",
+    "Wells",
+    "parse_well_ids_plaintext",
+    "parse_well_ids",
+    "fetch_dh_doc_image_urls",
+    "WaterConnectSession",
+    "Response",
+)
