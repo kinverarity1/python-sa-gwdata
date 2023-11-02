c16ab6a3f60a7c9cca0116167245e05a091ba936
Kent Inverarity <kent@nowhere>
2023-11-02 13:20:37 +1030

    Fix #33 - __version__ now using importlib.metadata

--- sa_gwdata/__init__.py
+++ sa_gwdata/__init__.py
@@ -1,11 +1,14 @@
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("python-sa-gwdata")
+except PackageNotFoundError:
+    __version__ = "unknown version"
+
 from sa_gwdata.waterconnect import *
 from sa_gwdata.waterconnect_funcs import *
 from sa_gwdata.sarig import *
 from sa_gwdata.identifiers import *
 from sa_gwdata.local_cache import *
-
-from .version import version
-
-__version__ = version()
 
 cache = LocalCache()
